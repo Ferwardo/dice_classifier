3ff79c257917af1a6d5ac6852a90ff726bcaaf84
Ferwardo <ferresax@hotmail.com>
2022-10-09 15:20:35 +0200

    added conversion to tflite format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
 from keras.layers import Rescaling, Conv2D, BatchNormalization, MaxPool2D, Dense, Flatten, Dropout
-import numpy as np
+import tensorflow_model_optimization as tfmot
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -25,12 +25,13 @@ train_dataset = tf.keras.utils.image_dataset_from_directory(dataset_path + "/tra
 test_dataset = tf.keras.utils.image_dataset_from_directory(dataset_path + "/valid", image_size=(480, 480),
                                                            seed=123, batch_size=32)
 
+# caches images
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_dataset = train_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 test_dataset = test_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
-# define the model, used architecture is AlexNet
+# define the model, used architecture is AlexNet more info see https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
 model = tf.keras.models.Sequential([
     Rescaling(1. / 255),  # rescale the colour of the image so it is between 0 and 1
     Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation='relu', input_shape=(227, 227, 3)),
@@ -59,6 +60,7 @@ model = tf.keras.models.Sequential([
     Dense(4, activation='softmax')
 ])
 
+# compile and fit
 model.compile(optimizer="adam",
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=["accuracy"])
@@ -71,3 +73,21 @@ model.fit(
     batch_size=32)
 
 model.summary()
+
+# quantise the model
+quantize_model = tfmot.quantization.keras.quantize_model
+quantized_model = quantize_model(model)
+quantized_model.compile(optimizer='adam',
+                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                        metrics=['accuracy'])
+
+# train the quantised model
+quantized_model.fit(train_dataset, validation_split=0.25, batch_size=32, epochs=5)
+quantized_model.evaluate(test_dataset, verbose=2)
+
+# convert to tflite format
+converter = tf.lite.TFLiteConverter.from_keras_model(quantized_model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tf_model = converter.convert()
+with open('dice_classifier.tflite', 'wb') as f:
+    f.write(tf_model)
